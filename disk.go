788caa4a473ba4f83132b239935898873986a5fb
Lloyd Hiaiel <lth@fb.com>
2020-02-13 11:35:02 -0700

    add accessors for external storage status and doc to .Disk

--- disk.go
+++ disk.go
@@ -23,6 +23,7 @@ type Disk struct {
 	rMask                   uint64
 	f                       *os.File
 	filterRead, storageRead extReader
+	storageBits             uint
 }
 
 // OpenReadOnlyFromFile initializes a read only quotient filter
@@ -40,6 +41,7 @@ func OpenReadOnlyFromPath(path string) (*Disk, error) {
 	var ext Disk
 	ext.entries = h.Entries
 	ext.rBits, ext.rMask, ext.size = initForQuotientBits(uint(h.QBits))
+	ext.storageBits = uint(h.StorageBits)
 	if h.BitPacked {
 		ext.filterRead, err = initPackedDiskReader(rdr)
 		if err != nil {
@@ -68,6 +70,19 @@ func OpenReadOnlyFromPath(path string) (*Disk, error) {
 	return &ext, nil
 }
 
+// StorageBits reports the number of bits of integer storage associated
+// with each entry in the quotient filter
+func (ext *Disk) StorageBits() uint {
+	return ext.storageBits
+}
+
+// HasStorage is true when a non-zero amount of integer storage is tracked
+// along with each entry in the quotient filter
+func (ext *Disk) HasStorage() bool {
+	return ext.storageBits > 0
+}
+
+// Close the file handle associated with the disk based quotient filter
 func (ext *Disk) Close() error {
 	if ext.f != nil {
 		return ext.f.Close()
@@ -75,20 +90,25 @@ func (ext *Disk) Close() error {
 	return nil
 }
 
+// Len reports the number of entries in the quotient filter
 func (ext *Disk) Len() uint64 {
 	return ext.entries
 }
 
+// Contains checks whether the byte string is stored within the quotient filter
 func (ext *Disk) Contains(v []byte) bool {
 	found, _ := ext.Lookup(v)
 	return found
 }
 
+// Contains checks whether the string is stored within the quotient filter
 func (ext *Disk) ContainsString(s string) bool {
 	found, _ := ext.Lookup(*(*[]byte)(unsafe.Pointer(&s)))
 	return found
 }
 
+// Lookup checks whether the byte string is stored within the quotient filter and
+// returns a boolean indicating its presence and external integer data if applicable
 func (ext *Disk) Lookup(key []byte) (bool, uint64) {
 	dq, dr := hash(ext.hashfn, key, ext.rBits, ext.rMask)
 
@@ -112,6 +132,7 @@ func (ext *Disk) Lookup(key []byte) (bool, uint64) {
 	return lookupByHash(dq, dr, ext.size, filterFn, storageFn)
 }
 
+// LookupString is like Lookup, but for strings
 func (ext *Disk) LookupString(key string) (bool, uint64) {
 	return ext.Lookup(*(*[]byte)(unsafe.Pointer(&key)))
 }
